0bb0d8a1f79084b446f64d734b0484b509c0c54d
Aashish Joshi <theeaashish@gmail.com>
2025-11-04 10:56:26 +0000

    feat: add the read file and directory

--- README.md
+++ README.md
@@ -1,15 +1,8 @@
-# fs
+What is Bun?
+Bun is an all-in-one toolkit for JavaScript and TypeScript apps.
+It ships as a single executable called bun.
 
-To install dependencies:
-
-```bash
-bun install
-```
-
-To run:
-
-```bash
-bun run index.ts
-```
-
-This project was created using `bun init` in bun v1.2.20. [Bun](https://bun.com) is a fast all-in-one JavaScript runtime.
+At its core is the Bun runtime, a fast JavaScript runtime designed as
+a drop-in replacement for Node.js. It's written in Zig and powered by
+JavaScriptCore under the hood, dramatically reducing startup times
+and memory usage.
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
 
 const app = express();
 
@@ -10,37 +10,26 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  fs.readdir(`${__dirname}/`, (err, data) => {
+  fs.readdir(__dirname, (err, data) => {
     res.json({
-        data
-    })
+      data,
+    });
   });
 });
 
 app.post('/', (req, res) => {
   const { name } = req.body;
-
   fs.readdir(`${__dirname}/${name}`, (err, data) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.json({
-        data
-    })
+    res.json({ data });
   });
-})
+});
 
 app.post('/file', (req, res) => {
   const { name } = req.body;
 
-  fs.readFile(`${__dirname}/${name}`, 'utf8', (err, data) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.json({
-        content: data
-    })
+  fs.readFile(`${__dirname}/${name}`, 'utf-8', (err, data) => {
+    res.json({ content: data });
   });
-})
+});
 
 app.listen(PORT);
